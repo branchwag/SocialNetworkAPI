8769b9baed0f96e9956b41e8b8aa742153d9f51d
LeAnne B <branchleannelmb@gmail.com>
2023-07-14 08:57:09 -0400

    removes commented out code

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,3 @@
 const router = require("express").Router();
-// const videoRoutes = require('./videoRoutes');
-// const userRoutes = require('./userRoutes');
-
-// router.use('/videos', videoRoutes);
-// router.use('/users', userRoutes);
 
 module.exports = router;
