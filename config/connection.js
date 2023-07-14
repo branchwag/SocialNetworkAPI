c5ba7557ade03462c83ecd2fd2a7536297b0d47e
LeAnne B <branchleannelmb@gmail.com>
2023-07-14 08:45:13 -0400

    adds in connection file

--- config/connection.js
+++ config/connection.js
@@ -3,3 +3,9 @@
 //connect to mongoose
 
 //export connection
+
+const { connect, connection } = require("mongoose");
+
+connect("mongodb://127.0.0.1:27017/socialNetworkDB");
+
+module.exports = connection;
